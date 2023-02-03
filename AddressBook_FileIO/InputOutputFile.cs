c131ff4598e316cd7422a20fd5e55c2c95d677d4
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-02-03 21:35:50 +0530

    Added File I/O to the Address Book Program

---
+++ AddressBook_FileIO/InputOutputFile.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook_FileIO
+{
+    public  class InputOutputFile
+    {
+        public string path;
+        public InputOutputFile()
+        {
+            path = @"C:\Users\sai\source\repos\RFP-244\AddressBook_FileIO\AddressBook_I-OFile\AddressBook_FileIO\ContactDetails.txt";
+        }
+        public void CheckIfExist()
+        {
+            if(!File.Exists(path))
+            {
+                return;
+            }
+        }
+        public  void WriteDetailsInFile(string path, PersonDetails person)
+        {
+
+            if (File.Exists(path))
+            {
+                StreamWriter sw = File.AppendText(path);
+                sw.WriteLine("\nFirst Name : " + person.FirstName);
+                sw.WriteLine("Last Name : " + person.LastName);
+                sw.WriteLine("Address : " + person.Address);
+                sw.WriteLine("City : " + person.City);
+                sw.WriteLine("State : " + person.State);
+                sw.WriteLine("Email : " + person.Email);
+                sw.WriteLine("Zip code : " + person.Zip);
+                sw.WriteLine("PhoneNumber : " + person.PhoneNumber);
+                sw.WriteLine();
+                sw.Close();
+                Console.WriteLine("\nPerson Details Added SuccessFully In File");
+            }
+            else
+            {
+                Console.WriteLine("\nFile Not Found");
+            }
+        }
+        public void ReadDetailsFromFile(string path)
+        {
+            if (File.Exists(path))
+            {
+                Console.WriteLine(File.ReadAllText(path));
+            }
+            else
+            {
+                Console.WriteLine("\nFile Not Found");
+            }
+        }
+    }
+}
--- AddressBook_FileIO/Program.cs
+++ AddressBook_FileIO/Program.cs
@@ -5,6 +5,33 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Dispalaying File Input Output Stream Reader ");
+
+            string path = @"C:\Users\sai\source\repos\RFP-244\AddressBook_FileIO\AddressBook_I-OFile\AddressBook_FileIO\ContactDetails.txt";
+
+            PersonDetails details = new PersonDetails();
+            Console.WriteLine("\nEnter your First Name : ");
+            details.FirstName = Console.ReadLine();
+            Console.WriteLine("Enter your Last Name : ");
+            details.LastName = Console.ReadLine();
+            Console.WriteLine("Enter your Address : ");
+            details.Address = Console.ReadLine();
+            Console.WriteLine("Enter your City Name : ");
+            details.City = Console.ReadLine();
+            Console.WriteLine("Enter your State Name : ");
+            details.State = Console.ReadLine();
+            Console.WriteLine("Enter your Zip Code : ");
+            details.Zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your Phone Number : ");
+            details.PhoneNumber = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter your Email Address: ");
+            details.Email = Console.ReadLine();
+
+            InputOutputFile inputOutput = new InputOutputFile();
+            inputOutput.CheckIfExist();
+            inputOutput.WriteDetailsInFile(path, details );
+            inputOutput.ReadDetailsFromFile(path);
+
+
         }
     }
 }
